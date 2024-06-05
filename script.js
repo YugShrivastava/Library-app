9c91602a48688b5e5802ce4e51f14b1968ab46cf
Yug <shrivastavayug04@gmail.com>
2024-06-05 22:33:18 +0530

    Ran into bugs trying to fixed them.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // function books(title, author, pages, read) {
 //     this.title = title;
 //     this.author = author;
@@ -15,24 +14,45 @@
 //     "not read yet"
 //   );
 //   console.log(harryPotter.info());
-
+const wrapperDiv = document.querySelector(".wrapper");
 const bookArray = [];
 
-function book(title, author, pages, read){
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read,
-    this.info = function () {return `${title} by ${author}, ${pages} pages, ${read}`};
+function book(title, author, pages, read, thiss) {
+  (thiss.title = title),
+    (thiss.author = author),
+    (thiss.pages = pages),
+    (thiss.read = read),
+    (thiss.info = function () {
+      return `${title} by ${author}, ${pages} pages, ${read}`;
+    });
 }
 
-function addBookToLibrary(){
-    let title = prompt("Title of the book: ");
-    let author = prompt(`Author of ${title}: `);
-    let pages = prompt(`Number of pages in ${title}: `);
-    let read = prompt(`Have you read ${title} yet: `);
+// bookArray.push(
+//   book("A Brief History Of Time", "Stephen Hawking", 256, "not read")
+// );
+// bookArray.push(book("The Shining", "Stephen King", 505, "read"));
+// bookArray.push(book("Atomic Habits", "James Clear", 320, "reading"));
 
-    let currBook = new book(title, author, pages, read);
+function addBookToLibrary() {
+  let title = prompt("Title of the book: ");
+  let author = prompt(`Author of ${title}: `);
+  let pages = prompt(`Number of pages in ${title}: `);
+  let read = prompt(`Have you read ${title} yet: `);
 
-    bookArray.push(currBook);
+  let currBook = new book(title, author, pages, read, this);
+
+  bookArray.push(currBook);
 }
+
+function addToDisplay(arr) {
+  arr.forEach((book) => {
+    const divDisplay = document.createElement("div");
+    divDisplay.setAttribute("id", "bookDivs");
+    divDisplay.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}, Status: ${book.read}`;
+    wrapperDiv.appendChild(divDisplay);
+  });
+}
+
+addBookToLibrary();
+console.log(bookArray);
+addToDisplay(bookArray);
