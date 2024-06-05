ff170e7bbc07d7e74961e073e0e326a0472e6d44
Yug <shrivastavayug04@gmail.com>
2024-06-05 23:41:26 +0530

    Minor changes.

--- script.js
+++ script.js
@@ -15,20 +15,33 @@
 //   );
 //   console.log(harryPotter.info());
 const wrapperDiv = document.querySelector(".wrapper");
-const bookArray = [];
+const bookArray = [{
+    title: "The Shining",
+    author: "Stephen King",
+    pages: 505,
+    read: "Not Read",
+}];
 
-function book(title, author, pages, read, thiss = this) {
-  (thiss.title = title),
-    (thiss.author = author),
-    (thiss.pages = pages),
-    (thiss.read = read),
-    (thiss.info = function () {
+function book(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read),
+    (this.info = function () {
       return `${title} by ${author}, ${pages} pages, ${read}`;
     });
 }
 
-bookArray.push(
-  book("A Brief History Of Time", "Stephen Hawking", 256, "not read")
-);
-bookArray.push(book("The Shining", "Stephen King", 505, "read"));
-bookArray.push(book("Atomic Habits", "James Clear", 320, "reading"));
+// bookArray.push(
+//   book("A Brief History Of Time", "Stephen Hawking", 256, "not read")
+// );
+// bookArray.push(book("The Shining", "Stephen King", 505, "read"));
+// bookArray.push(book("Atomic Habits", "James Clear", 320, "reading"));
+
+function addToBookArray(bookArray){
+    bookArray.array.forEach(book => {
+        const displayDiv = document.createElement("div")
+        wrapperDiv.appendChild(displayDiv);
+    });
+}
+addToBookArray(bookArray);
